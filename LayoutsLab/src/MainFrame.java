fa1ff5d23327f7c1fa7862fae3220ca1138986b7
David Boivin ~Spit <db2_hockey@hotmial.com>
2014-09-27 15:22:47 -0400

    Set up the Main frame

---
+++ LayoutsLab/src/MainFrame.java
@@ -0,0 +1,50 @@
+
+
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author David Boivin (Spit)
+ */
+public class MainFrame extends JFrame {
+	
+// Constants --------------------------------------------------------------- //
+	
+	public static final String TITLE = "Layouts MainFrame";
+	
+	private final Dimension FRAME_DIMEN = new Dimension(500,500);
+	
+// Members ----------------------------------------------------------------- //
+	
+	
+	
+// Constructors ------------------------------------------------------------ //
+	
+	public MainFrame(){
+		super(TITLE);
+		
+		this.setPreferredSize(FRAME_DIMEN);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		
+		this.setLayout(new BorderLayout(5,5));
+		
+		this.setVisible(true);
+		//this.pack();
+	}
+	
+// Private Methods --------------------------------------------------------- //
+	
+	
+	
+// MAIN -------------------------------------------------------------------- //
+	
+	public static void main(String[] args){
+		
+		new MainFrame();
+	}
+}
