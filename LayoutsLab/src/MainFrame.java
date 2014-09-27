3bba78d202afd24a33a92167ea4a12942d3802c3
David Boivin ~Spit <db2_hockey@hotmial.com>
2014-09-27 17:41:53 -0400

    -refactoring

--- LayoutsLab/src/MainFrame.java
+++ LayoutsLab/src/MainFrame.java
@@ -14,51 +14,51 @@ import javax.swing.JFrame;
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
 	
-	// Constants --------------------------------------------------------------- //
+// Constants --------------------------------------------------------------- //
+
+	public static final String TITLE = "Layouts MainFrame";
 	
-		public static final String TITLE = "Layouts MainFrame";
+	private final Dimension FRAME_DIMEN = new Dimension(500,500);
 		
-		private final Dimension FRAME_DIMEN = new Dimension(500,500);
+// Members ----------------------------------------------------------------- //
+	
+	
+	
+// Constructors ------------------------------------------------------------ //
+	
+	public MainFrame(){
+		super(TITLE);
 		
-	// Members ----------------------------------------------------------------- //
+		//start set up of the main frame variables
+		this.setPreferredSize(FRAME_DIMEN);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
 		
+		//setup the frame content
+		this.setupContent();
 		
-		
-	// Constructors ------------------------------------------------------------ //
-		
-		public MainFrame(){
-			super(TITLE);
-			
-			//start set up of the main frame variables
-			this.setPreferredSize(FRAME_DIMEN);
-			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			this.setLocationRelativeTo(null);
-			
-			//setup the frame content
-			this.setupContent();
-			
-			//finish setting up the main frame variables
-			this.setVisible(true);
-			//this.pack();
-		}
-		
-	// Private Methods --------------------------------------------------------- //
-		
-		private void setupContent(){
-			
-			//set Layout
-			this.setLayout(null);
-			
-			//add panels to the frame
-			this.add(new Panel1());
-			this.add(new Panel2());
-			this.add(new Panel3());
-		}
-		
-	// MAIN -------------------------------------------------------------------- //
-		
-		public static void main(String[] args){
-			
-			new MainFrame();
-		}
+		//finish setting up the main frame variables
+		this.setVisible(true);
+		//this.pack();
 	}
+	
+// Private Methods --------------------------------------------------------- //
+	
+	private void setupContent(){
+		
+		//set Layout
+		this.setLayout(null);
+		
+		//add panels to the frame
+		this.add(new Panel1());
+		this.add(new Panel2());
+		this.add(new Panel3());
+	}
+	
+// MAIN -------------------------------------------------------------------- //
+	
+	public static void main(String[] args){
+		
+		new MainFrame();
+	}
+}
